27cff71736e15f73f3bb7747939ac1a6edcf104b
Peter Girard <pgirard@appgeo.com>
2016-06-29 10:01:23 -0400

    added Web Mercator extent substitutions in tile URL templates

--- WebApp/Scripts/leaflet-gpv-extensions.js
+++ WebApp/Scripts/leaflet-gpv-extensions.js
@@ -60,9 +60,9 @@
   // Map extensions
 
   L.Polygon = L.Polygon.extend({
-	  _convertLatLngs: function (latlngs) {
-		  return L.Polyline.prototype._convertLatLngs.call(this, latlngs);
-	  },
+    _convertLatLngs: function (latlngs) {
+      return L.Polyline.prototype._convertLatLngs.call(this, latlngs);
+    },
   });
 
   function unproject(map, p) {
@@ -306,6 +306,42 @@
       }
     }
   });
+
+  // TileLayer extension
+  
+  // add support for Web Mercator extent and tile size in tile URL template
+
+  L.TileLayer.prototype.getTileUrl = function (coords) {
+    var d = 20037508.342787;
+    var z = this._getZoomForUrl();
+    var w = d / Math.pow(2, z - 1);
+    var minx = -d + coords.x * w;
+    var miny = d - (coords.y + 1) * w;
+
+    var data = {
+      r: L.Browser.retina ? '@2x' : '',
+      s: this._getSubdomain(coords),
+      x: coords.x,
+      y: coords.y,
+      z: z,
+      minx: minx,
+      miny: miny,
+      maxx: minx + w,
+      maxy: miny + w
+    };
+
+    if (this._map && !this._map.options.crs.infinite) {
+      var invertedY = this._globalTileRange.max.y - coords.y;
+      
+      if (this.options.tms) {
+        data['y'] = invertedY;
+      }
+
+      data['-y'] = invertedY;
+    }
+
+    return L.Util.template(this._url, L.extend(data, this.options));
+  };
 
   // Text
 
